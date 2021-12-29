3856aab9c4b90293e1f4e420cfd4c3865ce0342f
Igor <trle1996@gmail.com>
2021-12-30 00:40:52 +0100

    implement socket io for change cards positions

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 dotenv.config({ path: __dirname + "/config.env" });
 
 const app = require("./app");
+const httpServer = createServer(app);
 
 const DB = process.env.DATABASE_LOCAL;
 
@@ -19,8 +22,27 @@ mongoose
   .then((connection) => console.log("DB connection successful!"))
   .catch((err) => console.log(`DB connection ERROR!!! ${err}`));
 
+// set Front domain
+const io = new Server(httpServer, {
+  cors: { origin: "http://localhost:3000" },
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-board", (boardId) => {
+    socket.join(boardId);
+  });
+
+  socket.on("leave-board", (boardId) => {
+    socket.leave(boardId);
+  });
+
+  socket.on("change-card-position", (result, boardId) => {
+    socket.to(boardId).emit("change-position", result);
+  });
+});
+
 // SERVER
 const port = process.env.PORT || 5000;
-const server = app.listen(port, () => {
+httpServer.listen(port, () => {
   console.log(`App running on port ${port} in ${process.env.NODE_ENV} mode...`);
 });
